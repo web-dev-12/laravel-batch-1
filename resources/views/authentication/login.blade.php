fd6f513292d8d446c2bc19adadd7756447d563c2
NHIPP <NHIPP@nh.ctg>
2022-05-28 18:35:31 +0600

    User Registration

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -13,9 +13,9 @@
                                         <a href="index.html"><img src="images/logo-full.png" alt=""></a>
                                     </div>
                                     @if(Session::has('response'))
-                                        <div class="alert alert-danger solid alert-dismissible fade show">
+                                        <div class="alert alert-success solid alert-dismissible fade show">
                                             <svg viewBox="0 0 24 24" width="24 " height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="me-2"><polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
-                                            <strong>Error!</strong>{{Session::get('response')['message']}}
+                                            <strong>Success!</strong>{{Session::get('response')['message']}}
                                             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="btn-close">
                                             </button>
                                         </div>
@@ -56,7 +56,7 @@
                                         </div>
                                     </form>
                                     <div class="new-account mt-3">
-                                        <p>Don't have an account? <a class="text-primary" href="page-register.html">Sign up</a></p>
+                                        <p>Don't have an account? <a class="text-primary" href="{{route('registration_form')}}">Sign up</a></p>
                                     </div>
                                 </div>
                             </div>
---
+++ resources/views/authentication/signUp.blade.php
@@ -0,0 +1,54 @@
+@extends('layout.auth')
+@section('title',"Wallet Login")
+@section('content')
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+                                    <div class="text-center mb-3">
+                                        <a href="index.html"><img src="images/logo-full.png" alt=""></a>
+                                    </div>
+                                    
+                                    <h4 class="text-center mb-4">Sign in your account</h4>
+                                    
+                                    <form action="{{route('registration')}}" method="post">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label for="name" class="mb-1"><strong>Name</strong></label>
+                                            <input type="text" name="name" id="name" class="form-control">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="username" class="mb-1"><strong>Username</strong></label>
+                                            <input type="text" name="username" id="username" class="form-control">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="email" class="mb-1"><strong>Email</strong></label>
+                                            <input type="text" name="email" id="email" class="form-control">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="mobileNumber" class="mb-1"><strong>Mobile Number</strong></label>
+                                            <input type="text" name="mobileNumber" id="mobileNumber" class="form-control">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="password" class="mb-1"><strong>Password</strong></label>
+                                            <input type="password" name="password" id="password" class="form-control">
+                                        </div>
+                                        
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary btn-block">Register</button>
+                                        </div>
+                                    </form>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
